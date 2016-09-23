8519ca417c5b08799a39cfd402026e8047679dce
lizichao <lzc121532>
2016-09-23 11:19:47 +0800

    add

--- src/cn/com/bright/workflow/aop/StartProcessInterceptor.java
+++ src/cn/com/bright/workflow/aop/StartProcessInterceptor.java
@@ -2,7 +2,7 @@ package cn.com.bright.workflow.aop;
 
 import org.aopalliance.intercept.MethodInterceptor ;
 import org.aopalliance.intercept.MethodInvocation;
-
+//无用
 public class StartProcessInterceptor implements MethodInterceptor{
 
 	public Object invoke(MethodInvocation invo) throws Throwable {
